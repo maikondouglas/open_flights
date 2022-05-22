3325fa9a41e1fe2d0d616b9ac50abb97a15928e4
Maikon Douglas <maikonconta0@gmail.com>
2022-05-22 15:37:37 -0300

    create seed and configuration models

---
+++ db/migrate/20220522180300_create_ailines.rb
@@ -0,0 +1,11 @@
+class CreateAilines < ActiveRecord::Migration[7.0]
+  def change
+    create_table :airlines do |t|
+      t.string :name
+      t.string :image_url
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
