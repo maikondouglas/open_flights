98e5442baa0f75cc3056d9dfe296d621d1304ce5
Maikon Douglas <maikonconta0@gmail.com>
2022-05-22 15:57:29 -0300

    created of routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only: %i[create destroy]
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all
 end
